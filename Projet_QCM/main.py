d317551979ab27f904f64d9d5479ce50665595b2
tommychtn <tommychanthanom@gmail.com>
2025-01-07 09:47:48 +0100

    Projet_QCM

--- Projet_QCM/main.py
+++ Projet_QCM/main.py
@@ -1,16 +1,28 @@
+#########################################################################
+##  main.py
+#########################################################################
+
+#########################################################################
+##  IMPORT
+#########################################################################
+
 from Projet_QCM.qcm import QCM
 from Projet_QCM.question import Question
 
+#########################################################################
+##  FONCTION MAIN
+#########################################################################
+
 def main():
-    # Créer les questions
+
+    # Création des questions
     questions = [
         Question("Quelle est la capitale de la France ?", ["Paris", "Lyon", "Marseille"], 0),
         Question("Combien font 2 + 2 ?", ["3", "4", "5"], 1),
         Question("Quelle est la couleur du ciel ?", ["Bleu", "Vert", "Rouge"], 0),
-        # Ajoutez plus de questions ici
     ]
     
-    # Initialiser le QCM
+    # Initialisation du QCM
     qcm = QCM(questions)
     qcm.shuffle_questions()
     qcm.ask_questions()
@@ -18,3 +30,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#########################################################################
+##  FIN main.py
+#########################################################################
