d97175310b2f64bfaa74d8272ebb2199b8411715
tommychtn <tommychanthanom@gmail.com>
2025-01-09 18:50:00 +0100

    Update main.py

--- Projet_QCM/main.py
+++ Projet_QCM/main.py
@@ -23,17 +23,18 @@ def main():
 
     # Creation des questions
     questions = [
-        Question("De quel pays est originaire la célèbre saucisse 'Bratwurst' ?", ["Allemagne", "Pologne", "Autriche"], 0),
-        Question("Quelle est la principale viande utilisée dans les saucisses de Francfort ?", ["Porc", "Bœuf", "Poulet"], 0),
-        Question("Comment appelle-t-on une saucisse sèche typique d'Espagne ?", ["Chorizo", "Salami", "Saucisson"], 0),
-        Question("Quel ingrédient est typiquement ajouté à une saucisse de Toulouse ?", ["Sel", "Herbes de Provence", "Ail"], 2),
-        Question("Dans quel plat britannique trouve-t-on des saucisses et de la purée ?", ["Bangers and Mash", "Shepherd's Pie", "Fish and Chips"], 0),
-        Question("Quelle saucisse est utilisée dans un 'Hot Dog' classique ?", ["Frankfurter", "Andouillette", "Merguez"], 0),
-        Question("La 'saucisse de Morteau' est une spécialité de quelle région française ?", ["Bourgogne", "Franche-Comté", "Alsace"], 1),
-        Question("Quel est le nom de la saucisse italienne souvent assaisonnée de fenouil ?", ["Salami", "Salsiccia", "Pancetta"], 1),
-        Question("Quelle est la particularité des saucisses 'vegan' ?", ["Elles sont faites de tofu", "Elles contiennent du poisson", "Elles n'ont pas de boyaux"], 0),
-        Question("Dans quel pays trouve-t-on la 'Boerewors', une saucisse traditionnelle ?", ["Afrique du Sud", "Australie", "Argentine"], 0),
-        ]
+        Question("Combien font 1 + 1 ?", ["1", "2", "3"], 1),
+        Question("Combien font 5 - 2 ?", ["3", "2", "4"], 0),
+        Question("Combien font 3 x 3 ?", ["6", "9", "12"], 1),
+        Question("Combien font 10 ÷ 2 ?", ["4", "5", "6"], 1),
+        Question("Combien font 7 + 3 ?", ["9", "10", "11"], 1),
+        Question("Combien font 12 - 4 ?", ["8", "7", "9"], 0),
+        Question("Combien font 6 x 2 ?", ["12", "14", "10"], 0),
+        Question("Combien font 15 ÷ 3 ?", ["4", "5", "6"], 1),
+        Question("Combien font 8 + 2 x 3 (suivant l'ordre des priorités) ?", ["30", "14", "10"], 1),
+        Question("Combien font 9 - 3 + 1 ?", ["5", "7", "6"], 2),
+    ]
+
     
     # Initialisation du QCM
     qcm = QCM(questions)
